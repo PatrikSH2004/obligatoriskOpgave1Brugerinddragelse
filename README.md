f5ee21dd84ec8ebfa963b607a6dabca32bad60a6
Patrik Serban Hansen <paha24ad@student.cbs.dk>
2026-09-14 12:04:13 +0200

    Tab navigator setup

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 ## to do
 - Få lavet overblik hvordan det skal struktureres app-vist.
 
-- Få spurgt indtil bruger feedback, obligatorisk forhold osv på mandag.
+### Announcement
+- Få et board som angiver begivenheder
+- Ved bruger, skal der være knapper hvor man kan tilføje en announcement
+- Ejendomstab --> Nyheder over ejendommen via administrationen eller viceværten
+- Beborer --> Mindre nyheder som folk kan slå op ved informations tabs
 
-- Få core elementer til at fungere først
+### Borrow Tools
+- En liste med værktøj eller redskaber, som kan lånes fra viceværten eller andre.
+- Holde styr på hvem har lånt hvad
+- Skal vise en grøn farve, hvis noget er tilgængeligt, og rødt hvis det ikke er tilgængeligt.
+
+### Basement Room
+- Skal vise en liste med ledige kælderrum
+- Grøn betyder frit, gul betyder reserveret, rød betyder optaget
--- obliOpg1/App.js
+++ obliOpg1/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import BasementRoom from './screens/basementRoom';
+import RentTools from './screens/toolRent';
+import Announcement from './screens/announcement';
+
+const tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <tab.Navigator>
+        <tab.Screen name="Announcements" component={Announcement} />
+        <tab.Screen name="Borrow Tools" component={RentTools}/>
+        <tab.Screen name="Basement Room" component={BasementRoom} />
+      </tab.Navigator>
+    </NavigationContainer>
   );
 }
 
